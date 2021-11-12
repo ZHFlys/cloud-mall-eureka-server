4ca648d84c2f0909923436c5a1dbcb6734653b4c
zhenghao <haoziu@163.com>
2021-11-12 18:58:05 +0800

    完成购物车、订单模块

---
+++ cloud-mall-cart-order/src/main/java/com/zh/cloud/mall/practice/cartorder/model/dao/CartMapper.java
@@ -0,0 +1,32 @@
+package com.zh.cloud.mall.practice.cartorder.model.dao;
+
+import com.zh.cloud.mall.practice.cartorder.model.pojo.Cart;
+import com.zh.cloud.mall.practice.cartorder.model.vo.CartVO;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+
+    List<CartVO> selectList(@Param("userId") Integer userId);
+
+    Cart selectCartByUserIdAndProductId(@Param("userId") Integer userId,
+            @Param("productId") Integer productId);
+
+    Integer selectOrNot(@Param("userId") Integer userId, @Param("productId") Integer productId,
+            @Param("selected") Integer selected);
+}
